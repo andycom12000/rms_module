67643def99830b899a4626a2ab749c3d5a951525
Yen-Lin Liu <andycom12000@gmail.com>
2016-03-02 20:31:23 -0600

    Basic module

--- mp2.c
+++ mp2.c
@@ -12,12 +12,11 @@
 #include <asm/uaccess.h>
 #include <linux/semaphore.h>
 #include <linux/timer.h>
-#include "mp1_given.h"
 #include "mp2_given.h"
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("12");
-MODULE_DESCRIPTION("CS-423 MP1");
+MODULE_DESCRIPTION("CS-423 MP2");
 
 #define DEBUG 1
 #define PID_LEN 10
@@ -36,49 +35,51 @@ struct Work{
 };
 
 // Function prototypes
-static ssize_t mp1_read(struct file *file, char __user *buffer, size_t count, loff_t *data);
-static int mp1_write(struct file *file, const char *buffer, unsigned long count, loff_t *data);
-void timer_callback(unsigned long data);
+static ssize_t mp2_read(struct file *file, char __user *buffer, size_t count, loff_t *data);
+static int mp2_write(struct file *file, const char *buffer, unsigned long count, loff_t *data);
 void scan(const char *str, const char *format, ...);
-static void work_func(struct work_struct *work);
 void update_list(int x);
 
+// API functions
+void registerProc(void);
+void unregisterProc(void);
+void yieldProc(void);
+
+
 // File Ops
-static const struct file_operations mp1_fops = {
+static const struct file_operations mp2_fops = {
 	.owner = THIS_MODULE,
-	.read = mp1_read,
-	.write = mp1_write,
+	.read = mp2_read,
+	.write = mp2_write,
 };
 
 // Global variable
-static struct proc_dir_entry *mp1_file;
-static struct proc_dir_entry *mp1_dir;
-static struct timer_list my_timer;
-static struct workqueue_struct *work_queue;
+static struct proc_dir_entry *mp2_file;
+static struct proc_dir_entry *mp2_dir;
 static char procfs_buffer[1024] = {'\0'};
 static char temp_buffer[1024] = {'\0'};
-static unsigned long procfs_buffer_size = 3;
+static unsigned long procfs_buffer_size = 0;
 struct Node mylist;
 static DEFINE_SEMAPHORE(list_lock);
 
 
-// mp1_init - Called when module is loaded
-int __init mp1_init(void)
+// mp2_init - Called when module is loaded
+int __init mp2_init(void)
 {
 	//int i = 0;
 	//struct Node *tmp = NULL;
 
 	#ifdef DEBUG
-  	printk(KERN_ALERT "MP1 MODULE LOADING\n");
+  	printk(KERN_ALERT "MP2 MODULE LOADING\n");
 	#endif
 
 	// Create directory and file entry
-	mp1_dir = proc_mkdir("mp1", NULL);
-	mp1_file = proc_create("mp1", 0666, mp1_dir, &mp1_fops);
-  	if(!mp1_file)
+	mp2_dir = proc_mkdir("mp2", NULL);
+	mp2_file = proc_create("status", 0666, mp2_dir, &mp2_fops);
+  	if(!mp2_file)
 	{
 		// Remove the directory before exiting
-		remove_proc_entry("mp1", NULL);
+		remove_proc_entry("mp2", NULL);
 		return -ENOMEM;
 	}
    
@@ -89,39 +90,25 @@ int __init mp1_init(void)
 	INIT_LIST_HEAD(&mylist.list);
 	
 	#ifdef DEBUG
-	printk(KERN_ALERT "Starting timer, fires per 5 secs\n");
-	#endif
-
-	setup_timer(&my_timer, timer_callback, 0);
-	mod_timer(&my_timer, jiffies + msecs_to_jiffies(5000));
-
-	#ifdef DEBUG
-	printk(KERN_ALERT "Creating workqueue\n");
-	#endif
-	work_queue = create_workqueue("mp1");
-	if(!work_queue)
-		printk("Failed to create workqueue\n");
-
-	#ifdef DEBUG
-	printk(KERN_ALERT "MP1 MODULE LOADED\n");
+	printk(KERN_ALERT "MP2 MODULE LOADED\n");
 	#endif
 
 	return 0;   
 }
 
-// mp1_exit - Called when module is unloaded
-void __exit mp1_exit(void)
+// mp2_exit - Called when module is unloaded
+void __exit mp2_exit(void)
 {
 	struct list_head *q, *pos;
 	struct Node *tmp;
 
 	#ifdef DEBUG
-	printk(KERN_ALERT "MP1 MODULE UNLOADING\n");
+	printk(KERN_ALERT "MP2 MODULE UNLOADING\n");
 	#endif
 
 	// Remove entries
-	remove_proc_entry("mp1", mp1_dir);
-	remove_proc_entry("mp1", NULL);
+	remove_proc_entry("status", mp2_dir);
+	remove_proc_entry("mp2", NULL);
 
 	// Deallocate linked list
 	printk(KERN_ALERT "Freeing list...\n");
@@ -133,24 +120,16 @@ void __exit mp1_exit(void)
 		kfree(tmp);
 	}
 
-	// Delete timer
-	printk(KERN_ALERT "Deleting timer\n");
-	del_timer(&my_timer);
-
-	printk(KERN_ALERT "Deleting workqueue\n");
-	flush_workqueue(work_queue);
-	destroy_workqueue(work_queue);
-
 	printk(KERN_ALERT "Done\n");
-	printk(KERN_ALERT "MP1 MODULE UNLOADED\n");
+	printk(KERN_ALERT "MP2 MODULE UNLOADED\n");
 }
 
 // Register init and exit funtions
-module_init(mp1_init);
-module_exit(mp1_exit);
+module_init(mp2_init);
+module_exit(mp2_exit);
 
 
-static ssize_t mp1_read(struct file *file, char __user *buffer, size_t count, loff_t *data)
+static ssize_t mp2_read(struct file *file, char __user *buffer, size_t count, loff_t *data)
 {
 	int ret;
 	struct Node *tmp = NULL;
@@ -185,9 +164,11 @@ static ssize_t mp1_read(struct file *file, char __user *buffer, size_t count, lo
 		return totalLength;
 }
 
-static int mp1_write(struct file *file, const char *buffer, unsigned long count, loff_t *data)
+static int mp2_write(struct file *file, const char *buffer, unsigned long count, loff_t *data)
 {
-	struct Node *tmp = NULL;
+	//struct Node *tmp = NULL;
+	char operation = '\0';
+	int pid = 0;
 
 	printk(KERN_ALERT "Writing...");
 	procfs_buffer_size = count;
@@ -203,37 +184,19 @@ static int mp1_write(struct file *file, const char *buffer, unsigned long count,
 #endif
 
 	// Put the pid and current time into linked list
-	tmp = (struct Node*)kmalloc(sizeof(struct Node), 0);
-	scan(procfs_buffer, "%d", &tmp->pid_i);
-	strncpy(tmp->pid, procfs_buffer, count-1);
-	tmp->pid[count-1] = '\0';
-	tmp->exec_time = 0;
-
+	//tmp = (struct Node*)kmalloc(sizeof(struct Node), 0);
+	scan(procfs_buffer, "%d%c", &pid, &operation);
+	//tmp->exec_time = 0;
+	/*
 	down(&list_lock);
 	list_add(&(tmp->list), &(mylist.list));
 	up(&list_lock);
+	*/
+#ifdef DEBUG
+	printk(KERN_ALERT "pid=%d, op=%c\n", pid, operation);
+#endif
 
 	return procfs_buffer_size;
-}
-
-void timer_callback(unsigned long data)
-{
-	struct Work *work;
-
-	printk(KERN_ALERT "timer_callback called (%ld)\n", jiffies);
-	mod_timer(&my_timer, jiffies + msecs_to_jiffies(5000));
-
-	if(work_queue)
-	{
-		work = (struct Work *)kmalloc(sizeof(struct Work), GFP_KERNEL);
-		if(work)
-		{
-			INIT_WORK((struct work_struct*)work, work_func);
-			queue_work(work_queue, (struct work_struct *)work);
-		}
-	}
-
-	//update_list();
 }
 
 void scan(const char *str, const char *format, ...)
@@ -246,13 +209,13 @@ void scan(const char *str, const char *format, ...)
 
 void update_list(int x)
 {
-	int ret = 0;
+	//int ret = 0;
 	struct Node *tmp = NULL;
 
 	down(&list_lock);
 	list_for_each_entry(tmp, &mylist.list, list)
 	{
-		ret = get_cpu_use(tmp->pid_i, &tmp->exec_time);
+		//ret = get_cpu_use(tmp->pid_i, &tmp->exec_time);
 #ifdef DEBUG
 		printk(KERN_ALERT "\tpid=%d, time=%lu\n", tmp->pid_i, tmp->exec_time);
 #endif
@@ -260,8 +223,23 @@ void update_list(int x)
 	up(&list_lock);
 }
 
-static void work_func(struct work_struct *work)
+void registerProc(void)
 {
-	update_list(0);
-	kfree((void *)work);
+#ifdef DEBUG
+	printk(KERN_ALERT "Register called\n");
+#endif
+}
+
+void unregisterProc(void)
+{
+#ifdef DEBUG
+	printk(KERN_ALERT "Unregister called\n");
+#endif
+}
+
+void yieldProc(void)
+{
+#ifdef DEBUG
+	printk(KERN_ALERT "Yield called\n");
+#endif
 }
